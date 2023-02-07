2a6d07d19c22c5967c0fa088c3c46b9b38627dd5
NishanthiBalan <119354873+NishanthiBalan@users.noreply.github.com>
2023-02-07 12:35:26 -0500

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 echo " Mysql installation started "
 sudo apt update
-sudo apt install mysql-server
+sudo apt install mysql-server -y
 sudo systemctl start mysql.service
 echo "Mysql installation done"
 Echo "End of the this script file"
